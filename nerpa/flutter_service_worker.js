ba99271f148ee389725a6ce20b8c1f0da6e8b26f
Nikita Cherkasov <trebbeat@gmail.com>
2022-08-10 22:22:47 +0300

    change routes

--- nerpa/flutter_service_worker.js
+++ nerpa/flutter_service_worker.js
@@ -44,9 +44,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "904d2e450e169bcbf5475b81de468a31",
-"/": "904d2e450e169bcbf5475b81de468a31",
-"main.dart.js": "77687c2cff2078d0b18eb5aebba3cf34",
+"index.html": "b66b46b637dd86491c0364a4ec53d9ce",
+"/": "b66b46b637dd86491c0364a4ec53d9ce",
+"main.dart.js": "de9a61c8e20a126f9bdc122604fc3fe0",
 "manifest.json": "f37332ab39fa6fecc5c295a6faf7309a",
 "version.json": "79d99cf536f8201c4f6e7d30979296ef"
 };
